321b20b9a120f8371f9825cdbb0f443d878608e6
Thomas, Emmanuel Sebastien <ethomas32@gatech.edu>
2017-09-29 15:39:35 -0700

    clean up repo

--- src/main/java/com/idreamon/mymoney/web/WebApplication.java
+++ src/main/java/com/idreamon/mymoney/web/WebApplication.java
@@ -2,10 +2,11 @@ package com.idreamon.mymoney.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.idreamon.mymoney.web"})
+//@EnableOAuth2Sso
 public class WebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
